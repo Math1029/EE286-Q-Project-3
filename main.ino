acace0dfc0cd53bdd4b794fdca59a10da86c5c54
Evan <paddock.evan@gmail.com>
2024-03-29 09:56:26 -0700

    Changed the distance function from int to double because the distance is returned as double not integer

--- main.ino
+++ main.ino
@@ -1,7 +1,7 @@
  #include <stdio.h>
  #include <math.h>
 
- int distance(double x, double y){
+ double distance(double x, double y){
     double distance = sqrt(pow(x,2) + pow(y,2));
     return distance;
  }

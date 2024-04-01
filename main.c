57b9c07ca4ea362ad2a42aca6fa435ba349f9e02
Evan <haastiin@protonmail.com>
2024-04-01 13:41:40 -0700

    Utilized all the functions into main to produce the keys

--- main.c
+++ main.c
@@ -17,18 +17,31 @@ double y_function(double x){
    return y;
 }
 
-double distance(double x, double y){
+double get_distance(double x, double y){
    double distance = sqrt(pow(x,2) + pow(y,2));
    return distance;
 }
 
-double key_generation(double distance, double private_key){
+double generate_key(double distance, int private_key){
    double public_key = distance*private_key;
    return public_key;
 }
 
 int main(){
    double x = x_random(-2, 2, 1001);
-   printf("%lf\n", x);
+   double y = y_function(x);
+   double distance = get_distance(x, y);
+
+   int key_a = 30;
+   int key_b = 40;
+
+   double key_one = generate_key(distance, key_a);
+   double key_two = generate_key(distance, key_b);
+
+   printf("distance: %lf\n", distance);
+   printf("key_a:    %d\n", key_a);
+   printf("key_one:  %lf\n", key_one);
+   printf("key_b:    %d\n", key_b);
+   printf("key_two:  %lf\n", key_two);
    return 0;
 }

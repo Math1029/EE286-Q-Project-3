293f9c216f95643a72b9da00d622de32ae92ea3b
Evan <paddock.evan@gmail.com>
2024-04-01 09:27:00 -0700

    Added key generation and y function

--- main.ino
+++ main.ino
@@ -1,7 +1,17 @@
- #include <stdio.h>
- #include <math.h>
+#include <stdio.h>
+#include <math.h>
 
- double distance(double x, double y){
-    double distance = sqrt(pow(x,2) + pow(y,2));
-    return distance;
- }
+double y_function(double x){
+   double y = sqrt(pow(x,3) - 3*x + 3);
+   return y;
+}
+
+double distance(double x, double y){
+   double distance = sqrt(pow(x,2) + pow(y,2));
+   return distance;
+}
+
+double key_generation(double distance, double private_key){
+   double public_key = private_key * distance;
+   return public_key;
+}
